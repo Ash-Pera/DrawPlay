bb8a3bd7abc7b7e435a7d8dbe3faa1828606c57e
Ash Pera <aepera@mines.eud>
2018-09-27 11:44:21 -0600

    added v fast cat

--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -8,20 +8,25 @@ public class MyDrawing extends JPanel {
 	private Cat cat;
 	private Cat cat2;
 	private Cat cat3;
+	private Cat running_cat;
 	public MyDrawing() 
 	{
 		// Add critters to the drawing
 		cat = new Cat();
 		cat2 = new Cat();
 		cat3 = new Cat();
-		
+		running_cat = new Cat();
 	}
+	
+	int running_cat_loc = 0;
 	// paintComponent is called automatically when the frame needs
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
 		cat.draw(g, 50, 50);
 		cat2.draw(g, 150, 150);
 		cat3.draw(g, 250, 25);
+		running_cat.draw(g, running_cat_loc, 300);
+		running_cat_loc++;
 	}
 	
 	/**
@@ -38,6 +43,10 @@ public class MyDrawing extends JPanel {
 		
 		// Frame will not display until you set visible true
 		frame.setVisible(true);
+		
+		while(true) {
+			frame.repaint();
+		}
 	}
 
 }
